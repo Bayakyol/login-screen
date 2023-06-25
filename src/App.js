7565cb7518e21611ce47bceaca1b83df0e70c542
Bayakyol <121816798+Bayakyol@users.noreply.github.com>
2023-06-26 00:12:45 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import Input from "./components/input/Input";
+import Button from './components/button/Button';
+import Form from "./components/form/Form";
 
 function App() {
+  const [user, setUser] = useState({ name: "", password: ""});
+const handleChange = (e) => {
+if(e.target.type==="text"){ 
+setUser({...user, name: e.target.value});
+console.log(user)
+}else if(e.target.type==="password")
+{ 
+  setUser({...user, password: e.target.value});
+  console.log(user);
+}
+};
+const handleClick=()=> {
+  if(user.name==="")
+  {
+    alert("Kullanıcı Adı Boş Bırakılamaz");
+  }
+  else if(user.password==="")
+  {
+    alert("Şifre Boş Bırakılamaz");
+  }
+  else {
+    alert("Kullanıcı Adı:" + user.name + " " + "Parolanız:" + user.password);
+  }
+};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Giriş Formu</h2>
+     <Form>
+     <Input type={"text"} value={user.name} placeHolder={"Kullanıcı Adı"} onChange={handleChange}/>
+     <Input type={"password"} value={user.password} placeHolder={"Şifre Giriniz"} onChange={handleChange}/>
+     <Button onClick={handleClick} />
+     </Form>
+     
     </div>
   );
 }
---
+++ src/components/button/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./button.css";
+
+function Button({onClick}) {
+  return <button className="button" onClick={onClick}>Giriş Yap</button>;
+}
+
+export default Button;
